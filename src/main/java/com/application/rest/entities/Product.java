2c966a68802ac6add76bc38f8e0d293d8133bdb4
4lejandroquintero <alejoq96@gmail.com>
2024-02-14 20:45:42 -0500

    creacion de entities

---
+++ src/main/java/com/application/rest/entities/Product.java
@@ -0,0 +1,32 @@
+package com.application.rest.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "producto")
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "nombre")
+    private String name;
+
+    @Column(name = "precio")
+    private BigDecimal price;
+
+    @ManyToOne
+    @JoinColumn(name = "id_fabricante", nullable = false)
+    private Maker maker;
+}
